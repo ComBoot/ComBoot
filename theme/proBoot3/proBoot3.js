7f121e28c6af2b679768c08542af8172e5777041
Florian Gareis <mail@zoker.me>
2014-03-25 18:28:59 +0100

    Fixed "to-top" link on posts #17 + added smooth scroll

--- theme/proBoot3/proBoot3.js
+++ theme/proBoot3/proBoot3.js
@@ -142,3 +142,10 @@ $(document).ready(function() {
 	$(".selectpicker").attr({"data-width": "auto"});
 	$(".selectpicker").selectpicker();
 });
+
+$(document).ready(function() {
+	$(".top").click(function(event){		
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 700);
+	});
+});
