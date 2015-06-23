233046fd30a76eb661ab5be1d3034162cc5d088a
Florian Gareis <mail@zoker.me>
2015-06-24 00:45:36 +0200

    Fix syntax highlighting issue #26

--- theme/comboot/comboot.js
+++ theme/comboot/comboot.js
@@ -247,3 +247,8 @@ $(document).on('click', '.panel-heading span.clickable', function(e){
 function isImageFile(fname) {
     return fname.match(/\.(jpg|gif|bmp|jpeg|png)$/);
 }
+
+Prism.hooks.add('before-highlight', function(env) {
+    env.element.innerHTML = env.element.innerHTML.replace(/<br\s*\/?>/g,'\n');
+    env.code = env.element.textContent.replace(/^(?:\r?\n|\r)/,'');
+});
